7d7c263664932bc6486b44603551e537e8f71cbf
Cem Samiloglu <cemsamiloglu@gmail.com>
2023-11-27 23:03:52 +0200

    Refactor methods

--- apriori-original.py
+++ apriori-original.py
@@ -1,52 +1,72 @@
 import csv
 from collections import defaultdict
 
-MINSUP = 0.6
+MINSUP = 0.2
 
-if __name__ == "__main__":
+def readFromInputFile(fileName):
     # extract rows from csv file
-    fileHandle = open("SMALL-DATASET.csv", "r")
-    rowRecords = set()
+    fileHandle = open(fileName, "r")
     for row in fileHandle:
         row = row.strip().rstrip(",")
-        rowRecords.add(row)
-    print("rowRecords in csv file: ", rowRecords)
+        rowRecords = frozenset(row.split(","))
+        yield rowRecords
+        print("rowRecords in csv file: ", rowRecords)
 
+def getItemSetWithMinSup(itemSet, transactionList, MINSUP, frequentItemSet):
+    localItemSet = set()
+    itemSetFrequency = defaultdict(int)
+
+    # calculate frequency of items in itemSet
+    for item in itemSet:
+        for transaction in transactionList:
+            if item.issubset(transaction):
+                frequentItemSet[item] += 1
+                itemSetFrequency[item] += 1
+
+    print("itemSetFrequency: ", itemSetFrequency)
+
+    # if item's frequency is bigger than support add to new set
+    for item, count in itemSetFrequency.items():
+        support = float(count) / len(transactionList)
+        print("Item: ", item, " support: ", support)
+        if support >= MINSUP:
+            localItemSet.add(item)
+
+    print("frequent k-itemSet: ", localItemSet)
+    return localItemSet
+
+def joinSet(itemSet, itemSetLength):
+        return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == itemSetLength])
+
+if __name__ == "__main__":
+    rowRecords = readFromInputFile("SMALL-DATASET.csv")
     # extract itemSet and transactionList
     transactionList = list()
     itemSet = set()
 
     for rowRecord in rowRecords:
-        transactionList.append(rowRecord)
-        for item in rowRecord.split(","):
-            itemSet.add(item)
+        transaction = frozenset(rowRecord)
+        transactionList.append(transaction)
+        for item in transaction:
+            if not ("T" in item):
+                itemSet.add(frozenset([item]))
 
     print("itemset: ", itemSet)
     print("transactionList: ", transactionList)
 
-    # calculate frequency of items in itemSet
-    frequencyItemSet = defaultdict(int)
+    frequentItemSet = defaultdict(int)
 
-    for item in itemSet:
-        for transaction in transactionList:
-            if (item in transaction.split(",")) and not ("T" in item):
-                frequencyItemSet[item] += 1
+    itemSetWithMinSup = getItemSetWithMinSup(itemSet, transactionList, MINSUP, frequentItemSet)
 
-    print("frequencyItemset: ", frequencyItemSet)
-
-    for item, count in frequencyItemSet.items():
-        support = float(count) / len(transactionList)
-        print("Item: ", item, " support: ", support)
-
-    # if item's frequency is bigger than support add to new set
-    frequentItemSet = set()
-    for item, count in frequencyItemSet.items():
-        support = float(count) / len(transactionList)
-        if support >= MINSUP:
-            frequentItemSet.add(item)
-
-    print("frequent 1-itemSet: ", frequentItemSet)
-
-    joinSet = set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == 2])
-
-    print(joinSet)
+    currentSet = frequentItemSet
+    lengthIter = 2
+    while currentSet != set([]):
+        currentSet = joinSet(currentSet, lengthIter)
+        if currentSet == set([]):
+            break
+        
+        print("joinSet: ", currentSet)
+        itemSetWithMinSup = getItemSetWithMinSup(currentSet, transactionList, MINSUP, frequentItemSet)
+        print("For length: ", lengthIter, " itemSetWithMinSup: ", itemSetWithMinSup)
+        currentSet = itemSetWithMinSup
+        lengthIter += 1
